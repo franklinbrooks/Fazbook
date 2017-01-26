07c174a0005b2dda6282127b6447c2933e441e7f
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-01-26 16:53:17 -0500

    Add users show page

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/users/new', users);
+app.use('/users/show', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
